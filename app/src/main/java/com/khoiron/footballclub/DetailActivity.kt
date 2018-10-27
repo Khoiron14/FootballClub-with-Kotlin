e50010fbff79dbf917b6083ff766b45ebb6f5d5f
khoiron <khoironarkade@gmail.com>
2018-10-28 05:21:32 +0700

    refactoring

--- app/src/main/java/com/khoiron/footballclub/DetailActivity.kt
+++ app/src/main/java/com/khoiron/footballclub/DetailActivity.kt
@@ -18,44 +18,41 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val item: FootballClub = intent.getParcelableExtra("item")
-        DetailActivityUI(item).setContentView(this)
+        detailActivityUI(item)
     }
 
-    class DetailActivityUI(val item: FootballClub) : AnkoComponent<DetailActivity> {
-        override fun createView(ui: AnkoContext<DetailActivity>) = with(ui) {
-            verticalLayout {
-                padding = dip(16)
+    private fun detailActivityUI(item: FootballClub) {
+        verticalLayout {
+            padding = dip(16)
 
-                imageView {
-                    item.image?.let { Picasso.get().load(it).into(this) }
-                }.lparams {
-                    gravity = Gravity.CENTER_HORIZONTAL
-                    width = 200
-                    height = 200
+            imageView {
+                item.image?.let { Picasso.get().load(it).into(this) }
+            }.lparams {
+                gravity = Gravity.CENTER_HORIZONTAL
+                width = 200
+                height = 200
 
-                    margin = dip(16)
-                }
+                margin = dip(16)
+            }
 
-                textView {
-                    text = item.name
-                    textSize = 18f
-                    textAlignment = View.TEXT_ALIGNMENT_CENTER
-                }.lparams {
-                    width = matchParent
-                    height = wrapContent
-                    bottomMargin = dip(16)
-                }
+            textView {
+                text = item.name
+                textSize = 18f
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }.lparams {
+                width = matchParent
+                height = wrapContent
+                bottomMargin = dip(16)
+            }
 
-                textView {
-                    text = item.description
-                    textSize = 16f
-                    textAlignment = View.TEXT_ALIGNMENT_CENTER
-                }.lparams {
-                    width = matchParent
-                    height = wrapContent
-                }
+            textView {
+                text = item.description
+                textSize = 16f
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }.lparams {
+                width = matchParent
+                height = wrapContent
             }
         }
-
     }
 }
