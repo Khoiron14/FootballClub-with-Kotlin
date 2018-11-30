78a63b18f70afe2bdd09bfdc792864018a94d42f
khoiron <khoironarkade@gmail.com>
2018-11-30 07:36:51 +0700

    add event presenter

---
+++ app/src/main/java/com/khoiron/footballapps/presenter/event/EventDetailPresenter.kt
@@ -0,0 +1,57 @@
+package com.khoiron.footballapps.presenter.event
+
+import com.google.gson.Gson
+import com.khoiron.footballapps.data.api.ApiRepository
+import com.khoiron.footballapps.data.api.TheSportDBApi
+import com.khoiron.footballapps.data.model.event.EventDetailResponse
+import com.khoiron.footballapps.data.model.team.TeamResponse
+import com.khoiron.footballapps.ui.event.EventDetailView
+import com.khoiron.footballapps.util.context.CoroutinesContextProvider
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+/**
+ * Created by Khoiron14 on 30/11/18.
+ */
+class EventDetailPresenter(
+    private val view: EventDetailView,
+    private val apiRepository: ApiRepository,
+    private val gson: Gson,
+    private val context: CoroutinesContextProvider = CoroutinesContextProvider()
+) {
+    fun getEventDetail(eventId: String?) {
+        view.showLoading()
+
+        GlobalScope.launch(context.main) {
+            val data = gson.fromJson(
+                apiRepository.doRequest(TheSportDBApi.getEventDetail(eventId)).await(),
+                EventDetailResponse::class.java
+            )
+
+            view.showEventDetail(data.event)
+            view.hideLoading()
+        }
+    }
+
+    fun getHomeTeam(teamId: String?) {
+        GlobalScope.launch(context.main) {
+            val data = gson.fromJson(
+                apiRepository.doRequest(TheSportDBApi.getTeamDetail(teamId)).await(),
+                TeamResponse::class.java
+            )
+
+            view.showHomeTeam(data.teams)
+        }
+    }
+
+    fun getAwayTeam(teamId: String?) {
+        GlobalScope.launch(context.main) {
+            val data = gson.fromJson(
+                apiRepository.doRequest(TheSportDBApi.getTeamDetail(teamId)).await(),
+                TeamResponse::class.java
+            )
+
+            view.showAwayTeam(data.teams)
+        }
+    }
+}
---
+++ app/src/main/java/com/khoiron/footballapps/presenter/event/LastEventPresenter.kt
@@ -0,0 +1,34 @@
+package com.khoiron.footballapps.presenter.event
+
+import com.google.gson.Gson
+import com.khoiron.footballapps.data.api.ApiRepository
+import com.khoiron.footballapps.data.api.TheSportDBApi
+import com.khoiron.footballapps.data.model.event.EventResponse
+import com.khoiron.footballapps.ui.event.EventView
+import com.khoiron.footballapps.util.context.CoroutinesContextProvider
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+/**
+ * Created by Khoiron14 on 30/11/18.
+ */
+class LastEventPresenter(
+    private val view: EventView,
+    private val apiRepository: ApiRepository,
+    private val gson: Gson,
+    private val context: CoroutinesContextProvider = CoroutinesContextProvider()
+) {
+    fun getLastEventList(leagueId: String?) {
+        view.showLoading()
+
+        GlobalScope.launch(context.main) {
+            val data = gson.fromJson(
+                apiRepository.doRequest(TheSportDBApi.getLastEvents(leagueId)).await(),
+                EventResponse::class.java
+            )
+
+            view.showEventList(data.events)
+            view.hideLoading()
+        }
+    }
+}
