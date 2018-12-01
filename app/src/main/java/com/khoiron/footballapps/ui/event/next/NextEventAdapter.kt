e7644aa30d502570ce1abcec431ba427c4e3623b
khoiron <khoironarkade@gmail.com>
2018-12-01 17:59:57 +0700

    showing next event

---
+++ app/src/main/java/com/khoiron/footballapps/ui/event/next/NextEventAdapter.kt
@@ -0,0 +1,56 @@
+package com.khoiron.footballapps.ui.event.next
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.khoiron.footballapps.R
+import com.khoiron.footballapps.data.model.event.Event
+import com.khoiron.footballapps.util.convertDateTime
+import kotlinx.android.synthetic.main.item_event.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Khoiron14 on 01/12/18.
+ */
+class NextEventAdapter(private val events: List<Event>, private val listener: (Event) -> Unit) :
+    RecyclerView.Adapter<NextEventViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): NextEventViewHolder {
+        val view: View = LayoutInflater.from(p0.context).inflate(R.layout.item_event, p0, false)
+        val result = NextEventViewHolder(view)
+
+        view.setOnClickListener {
+            val position = result.adapterPosition
+
+            if (position != RecyclerView.NO_POSITION) {
+                val event: Event = events[position]
+                listener(event)
+            }
+        }
+
+        return result
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    override fun onBindViewHolder(p0: NextEventViewHolder, p1: Int) {
+        p0.bindItem(events[p1])
+    }
+
+}
+
+class NextEventViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun bindItem(events: Event) {
+        val formatDate = SimpleDateFormat("EEE, dd MMM yyyy", Locale.getDefault())
+        val formatTime = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val dateTimeConvert = convertDateTime(events.eventDate.toString(), events.eventTime)
+
+        itemView.txt_date.text = formatDate.format(dateTimeConvert)
+        itemView.txt_time.text = formatTime.format(dateTimeConvert)
+        itemView.txt_home_name.text = events.homeTeamName
+        itemView.txt_away_name.text = events.awayTeamName
+        itemView.txt_home_score.text = events.homeScore
+        itemView.txt_away_score.text = events.awayScore
+    }
+}
--- app/src/main/java/com/khoiron/footballapps/ui/event/next/NextEventFragment.kt
+++ app/src/main/java/com/khoiron/footballapps/ui/event/next/NextEventFragment.kt
@@ -1,29 +1,87 @@
 package com.khoiron.footballapps.ui.event.next
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-
+import com.google.gson.Gson
 import com.khoiron.footballapps.R
-import kotlinx.android.synthetic.main.fragment_last_event.*
+import com.khoiron.footballapps.data.api.ApiRepository
+import com.khoiron.footballapps.data.model.event.Event
+import com.khoiron.footballapps.presenter.event.NextEventPresenter
+import com.khoiron.footballapps.ui.event.EventView
+import kotlinx.android.synthetic.main.fragment_next_event.*
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.support.v4.toast
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class NextEventFragment : Fragment() {
+class NextEventFragment : Fragment(), EventView {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    private val events: MutableList<Event> = mutableListOf()
+    private var leagueId: String = "4328"
+    private lateinit var presenter: NextEventPresenter
+    private lateinit var adapter: NextEventAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val spinnerItems = resources.getStringArray(R.array.league_name)
         val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, spinnerItems)
         spinner.adapter = spinnerAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                        leagueId = "4328"
+                        presenter.getNextEventList(leagueId)
+                    }
+                    1 -> {
+                        leagueId = "4329"
+                        presenter.getNextEventList(leagueId)
+                    }
+                    2 -> {
+                        leagueId = "4331"
+                        presenter.getNextEventList(leagueId)
+                    }
+                    3 -> {
+                        leagueId = "4332"
+                        presenter.getNextEventList(leagueId)
+                    }
+                    4 -> {
+                        leagueId = "4334"
+                        presenter.getNextEventList(leagueId)
+                    }
+                    5 -> {
+                        leagueId = "4335"
+                        presenter.getNextEventList(leagueId)
+                    }
+                }
+            }
+        }
+
+        presenter = NextEventPresenter(this, ApiRepository(), Gson())
+        presenter.getNextEventList(leagueId)
+
+        adapter = NextEventAdapter(events) {
+            toast(it.homeTeamName.toString())
+        }
+
+        list_event.layoutManager = LinearLayoutManager(context)
+        list_event.adapter = adapter
+
+        swipe_refresh.onRefresh {
+            presenter.getNextEventList(leagueId)
+        }
     }
 
     override fun onCreateView(
@@ -34,5 +92,17 @@ class NextEventFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_next_event, container, false)
     }
 
+    override fun showLoading() {
+        swipe_refresh.isRefreshing = true
+    }
 
+    override fun hideLoading() {
+        swipe_refresh.isRefreshing = false
+    }
+
+    override fun showEventList(data: List<Event>) {
+        events.clear()
+        events.addAll(data)
+        adapter.notifyDataSetChanged()
+    }
 }
